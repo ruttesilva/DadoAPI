49f93b5024b52161ec37194cc2eb246cfff015fd
Rutte Oliveira <rosrosrutte@gmail.com>
2022-08-11 22:11:25 -0300

    Adicina frontend

--- Program.cs
+++ Program.cs
@@ -8,20 +8,23 @@ builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapGet("/dado/d{numeroDeFaces}", (
     [FromRoute] int numeroDeFaces
-) => {
+) =>
+{
     if (numeroDeFaces <= 0)
     {
-        return Results.BadRequest(new { mensagem = "Somente dados com pelo menos uma face"});
+        return Results.BadRequest(new { mensagem = "Somente dados com pelo menos uma face" });
     }
 
-    int face = RandomNumberGenerator.GetInt32(1, 7);
-
-    return Results.Ok (new { dado = $"d{numeroDeFaces}", rolagem = face });
+    int face = RandomNumberGenerator.GetInt32(1, numeroDeFaces + 1);
+    return Results.Ok(new { dado = $"d{numeroDeFaces}", rolagem = face });
 });
 
 app.Run();
